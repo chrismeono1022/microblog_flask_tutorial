bc759659503dfdfea54f13df9b0593a94b4ea7d3
Chris Meono <chrismeono@Chriss-MacBook-Air.local>
2013-04-18 19:46:07 -0700

    microblog flask megatutorial. finally committing old work.

---
+++ db_migrate.py
@@ -0,0 +1,10 @@
+#!flask/bin/python
+
+import imp
+from migrate.versioning import api
+from app import db
+from config import SQLALCHEMY_DATABASE_URI
+from confir import SQLALCHEMY_MIGRATE_REPO
+
+migration = SQLALCHEMY_MIGRATE_REPO + '/versions/%03d_migration.py' % (api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO) + 1)
+tmp_module = imp.new_module('old_model')
